6860312cdf3e8c90a20ffb4ea92204f38a55639d
BenjaminSegel <benjamin_segelstrom@hotmail.com>
2023-11-21 09:05:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Vi skapar två spelare, extra uppgift blir att göra det möjligt att spela mot dator. 
+// Skapa en boll som ska studsa tillbaka när det blir collision mellan spelare och boll
+// Åker bollen ut ur canvas, har man förlorat
+// Bollens hastighet ökar efter collissions, alltså blir spelet svårare beroende på hur många gånger bollen studsar. 
+// 1 collission = 1 poäng
+// Bollen kan endast åka ut på kortsidorna, och inte på långsidorna av canvas. 
+// Poäng och spelarnas namn sparar i local storage. 
